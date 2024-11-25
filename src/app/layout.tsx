df945cf0f529c4295bc6b4ae9a0f55323881320a
arun-murali0 <arunkarthick0810@gmail.com>
2024-11-25 11:34:44 +0530

    basic setup and assets are updtaed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,25 @@
 import type { Metadata } from 'next';
+import { Inter, IBM_Plex_Serif } from 'next/font/google';
 import './globals.css';
+import { ReactNode, FC } from 'react';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+	subsets: ['latin'],
+	variable: '--font-imb-plex-serif',
+	weight: ['400', '700'],
+});
 
 export const metadata: Metadata = {
-	title: 'Banking Application',
-	description: 'Banking application for payment ',
+	title: 'Swift Bank',
+	description:
+		'SwiftBank is a modern banking solution designed for secure account management, seamless transactions, and real-time financial insights. Built to simplify and innovate your banking experience.',
+	icons: './icons/logo.svg',
+};
+
+// Root Component
+const RootLayoutComponent: FC<{ children: ReactNode }> = ({ children }, className: string) => {
+	return <div className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</div>;
 };
 
 export default function RootLayout({
@@ -13,7 +29,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<RootLayoutComponent>{children}</RootLayoutComponent>
+			</body>
 		</html>
 	);
 }
